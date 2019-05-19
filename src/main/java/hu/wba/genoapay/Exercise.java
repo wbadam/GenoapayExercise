ee4deff1a384deda6a0f1433fe7b0e8bcf38b2b4
wbadam <wagadam@gmail.com>
2019-05-20 09:24:01 +1200

    Implement exercise and tests

--- src/main/java/hu/wba/genoapay/Exercise.java
+++ src/main/java/hu/wba/genoapay/Exercise.java
@@ -1,7 +1,41 @@
+package hu.wba.genoapay;
+
 public class Exercise {
 
+    /**
+     * Returns the maximum profit that can be made from the given stock prices. Throws {@link IllegalArgumentException}
+     * if stock prices are invalid.
+     *
+     * @param stockPrices an array of stock prices as positive integers
+     * @return maximum profit that can be made, or -1 if no profit can be made
+     */
     public int getMaxProfit(int[] stockPrices) {
 
-        return 0;
+        int profit = -1;
+        int buy = Integer.MAX_VALUE;
+        int sell = 0;
+
+        // Iterate through stock prices
+        for (int price : stockPrices) {
+
+            if (price <= 0) {
+                throw new IllegalArgumentException("Stock price must be a positive integer.");
+            }
+
+            if (price <= buy) {
+                // If current price is not higher than the buying price, start searching for profit from this minute.
+                // Calculate highest profit so far, set current as buying price, and reset selling price.
+                profit = Math.max(profit, sell - buy);
+                buy = price;
+                sell = 0;
+            } else if (price > sell) {
+                // If in the next minute the price is higher than the current highest selling price, update the selling
+                // price and calculate the profit.
+                sell = price;
+                profit = Math.max(profit, sell - buy);
+            }
+        }
+
+        return profit;
     }
 }
--- src/test/java/hu/wba/genoapay/ExerciseTest.java
+++ src/test/java/hu/wba/genoapay/ExerciseTest.java
@@ -4,11 +4,61 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class Exercise {
+public class ExerciseTest {
+
+    private Exercise exercise = new Exercise();
 
     @Test
-    public void getMaxProfitReturnsMaximumProfit() {
-        int[] stockPrices = {10, 7, 5, 8, 11, 9};
-        assertEquals(6, new Exercise().getMaxProfit(stockPrices));
+    public void calculatesMaximumProfit() {
+        int[] prices = {10, 7, 5, 8, 11, 9};
+        assertEquals(6, exercise.getMaxProfit(prices));
+    }
+
+    @Test
+    public void returnsMinusOneWhenNoProfitPossible() {
+        int[] prices = {10, 9, 8, 7, 6, 5};
+        assertEquals(-1, exercise.getMaxProfit(prices));
+    }
+
+    @Test
+    public void returnsMinusOneWhenPricesAreConstant() {
+        int[] prices = {10, 10, 10};
+        assertEquals(-1, exercise.getMaxProfit(prices));
+    }
+
+    @Test
+    public void returnsMinusOneWhenInputIsTooShort() {    // No time to sell stock
+        int[] prices = {10};
+        assertEquals(-1, exercise.getMaxProfit(prices));
+    }
+
+    @Test
+    public void returnsMinusOneWhenInputIsEmpty() {
+        int[] prices = {};
+        assertEquals(-1, exercise.getMaxProfit(prices));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionWhenStockPriceInvalid() {
+        int[] prices = {10, 12, 0, 8};
+        exercise.getMaxProfit(prices);
+    }
+
+    @Test
+    public void handlesExtremeValues() {
+        int[] prices = {Integer.MAX_VALUE, Integer.MAX_VALUE - 2, Integer.MAX_VALUE};
+        assertEquals(2, exercise.getMaxProfit(prices));
+    }
+
+    @Test
+    public void handlesExtremeConstantValues() {
+        int[] prices = {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE};
+        assertEquals(-1, exercise.getMaxProfit(prices));
+    }
+
+    @Test
+    public void handlesShortInputWithExtremeValue() {
+        int[] prices = {Integer.MAX_VALUE};
+        assertEquals(-1, exercise.getMaxProfit(prices));
     }
 }
